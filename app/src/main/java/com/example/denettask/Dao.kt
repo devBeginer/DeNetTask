c11903cb8351f1f361e3a1b5625d0f21770a6539
devBeginer <sFr0@ya.ru>
2022-08-07 01:18:55 +0300

    fixed methods

--- app/src/main/java/com/example/denettask/Dao.kt
+++ app/src/main/java/com/example/denettask/Dao.kt
@@ -7,17 +7,26 @@ import androidx.room.Dao
 @Dao
 interface Dao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertUser(node: DBNode)
+    suspend fun insertNode(node: DBNode)
 
     @Update
-    suspend fun updateUser(node: DBNode)
+    suspend fun updateNode(node: DBNode)
 
     @Delete
-    suspend fun deleteUser(node: DBNode)
+    suspend fun deleteNode(node: DBNode)
+
+    @Query("DELETE FROM DBNode")
+    suspend fun dropTable()
 
     @Query("SELECT * FROM DBNode WHERE name == :name")
     suspend fun getNodeByName(name: String): DBNode?
 
     @Query("SELECT * FROM DBNode WHERE id == :id")
     suspend fun getNodeById(id: Long): DBNode?
+
+    @Query("SELECT * FROM DBNode WHERE parent == :parent")
+    suspend fun getNodesByParent(parent: Long?): List<DBNode>
+
+    @Query("SELECT * FROM DBNode WHERE parent IS NULL")
+    suspend fun getRoot(): List<DBNode>
 }
