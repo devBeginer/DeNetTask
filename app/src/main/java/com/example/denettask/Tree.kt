1b0b2aec6b603874d8c97eabae452db26498cfd8
devBeginer <sFr0@ya.ru>
2022-08-07 01:16:33 +0300

    fixed 2 methods delete and iterator

--- app/src/main/java/com/example/denettask/Tree.kt
+++ app/src/main/java/com/example/denettask/Tree.kt
@@ -1,22 +1,26 @@
 package com.example.denettask
 
-class Tree(val root: TreeNode) {
+class Tree(val root: TreeNode = TreeNode(null)) {
     fun insertNode(parent: TreeNode): TreeNode{
         val newNode = TreeNode(parent)
         parent.children.add(newNode)
         return newNode
     }
 
+
     fun deleteNode(node: TreeNode){
         if (node != root){
             for (child in node.children) child.parent = node.parent
             node.parent!!.children.addAll(node.children)
+            node.parent!!.children.remove(node)
         }
     }
 
-
-    fun iteration(node: TreeNode){
-        println(node.name)
-        for (child in node.children) iteration(child)
+    fun iteration(node: TreeNode, onIteration: (node: TreeNode) -> Unit)/*: ArrayList<TreeNode>*/{
+        //val nodes = arrayListOf(node)
+        onIteration(node)
+        //for (child in node.children) nodes.addAll(iteration(child))
+        for (child in node.children) iteration(child, onIteration)
+        //return nodes
     }
 }
